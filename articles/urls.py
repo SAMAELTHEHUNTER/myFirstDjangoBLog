800baa009cbbc9e204c2524086662cb876eb1d2c
SAMAELTHRHUNTER <amirjalilibazel@mail.um.ac.ir>
2021-08-19 12:23:59 +0430

    adding article details page and changing some html codes

--- articles/urls.py
+++ articles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.articles_list),
+    path('', views.articles_list, name='articles_home'),
+    path('<slug>', views.article_details, name='details'),
 ]
